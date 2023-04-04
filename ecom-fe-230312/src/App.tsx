f441c8a5e44636067d6a0a682c37bbfddb8f2ebe
thanhtungha <59509509+thanhtungha@users.noreply.github.com>
2023-04-04 16:32:46 +0100

    [add] Initial Project

---
+++ ecom-fe-230312/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+	return <h1>Under development...</h1>;
+}
+
+export default App;
---
+++ ecom-fe-230312/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+		server: {
+			host: "localhost",
+			port: +env.VITE_PORT,
+			open: true,
+		},
+	};
+});
