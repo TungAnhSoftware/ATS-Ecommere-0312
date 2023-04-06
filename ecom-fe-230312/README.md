3781ad1bc13322ebcc2478eeee064fa1d3b1a2c3
dqdzung <dinhquangdung1011@gmail.com>
2023-04-06 19:30:53 +0700

    add lint, prettier,  readme

---
+++ ecom-fe-230312/README.md
@@ -0,0 +1,32 @@
+# FE Project Setup
+
+### Install pnpm
+
+```sh
+https://pnpm.io/installation
+```
+
+### Install dependencies
+
+```sh
+cd ecom-fe-230312
+pnpm i
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+pnpm run dev
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+pnpm run lint
+```
+
+### Format code with [Prettier](https://prettier.io/)
+
+```sh
+pnpm run prettier
+```
--- ecom-fe-230312/vite.config.ts
+++ ecom-fe-230312/vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import { fileURLToPath, URL } from "node:url";
-import dns from "dns";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
+import dns from 'dns';
 
-dns.setDefaultResultOrder("verbatim");
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd());
+  const env = loadEnv(mode, process.cwd());
 
-	return {
-		plugins: [react()],
-		resolve: {
-			alias: {
-				"@": fileURLToPath(new URL("./src", import.meta.url)),
-			},
-		},
-		server: {
-			host: "localhost",
-			port: +env.VITE_PORT,
-			open: true,
-		},
-	};
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: 'localhost',
+      port: +env.VITE_PORT,
+      open: true
+    }
+  };
 });
