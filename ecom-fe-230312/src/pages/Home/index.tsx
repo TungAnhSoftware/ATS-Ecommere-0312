4764866532e87b181e848768d4b082ecfafda505
hunghoang0809 <hoangmanhhung008009@gmail.com>
2023-05-19 23:09:28 +0700

    HomePage

--- ecom-fe-230312/src/pages/Home/index.tsx
+++ ecom-fe-230312/src/pages/Home/index.tsx
@@ -1,5 +1,27 @@
+// Components
+import Banner from './Components/Banner';
+import CarouselCard from './Components/Slides';
+//Matine core
+import { Anchor, Box, Text } from '@mantine/core';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* Header */}
+      {/* Navbar components */}
+      <Box mb={20}>
+        <Text fz="sm" ta="center" lineClamp={2}>
+          Get $200–$600 in credit toward iPhone 14 or iPhone 14 Pro when you
+          trade in iPhone 11 or higher.&nbsp;
+          <Anchor href="#">Shop iPhone &gt;</Anchor>
+        </Text>
+      </Box>
+      {/* Content */}
+      <Banner />
+      <CarouselCard />
+      {/* Footer */}
+    </>
+  );
 };
 
 export default Home;
