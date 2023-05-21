5a61b14b40083a8055fb8763273fd601fe3bc443
dqdzung <dinhquangdung1011@gmail.com>
2023-05-21 14:37:00 +0700

    ...

--- ecom-fe-230312/src/pages/ProductCart/ProductCart.tsx
+++ ecom-fe-230312/src/pages/ProductCart/ProductCart.tsx
@@ -1,9 +1,7 @@
-import Cart from "../../components/Cart/Cart";
+import Cart from '../../components/Cart/Cart';
 
 const ProductCart = () => {
-    return (
-        <Cart></Cart>
-    );
+  return <Cart />;
 };
-  
+
 export default ProductCart;
