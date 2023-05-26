ed5a75e7d701e87ca4e183399f9edb29cef13a1c
Thanh Tung Ha <tung.ht3011@gmail.com>
2023-05-26 18:27:12 +0100

    [fix] build gradle

--- ecom-be-230312/src/main/java/com/ecom/be/EcommerceApplication.java
+++ ecom-be-230312/src/main/java/com/ecom/be/EcommerceApplication.java
@@ -2,8 +2,10 @@ package com.ecom.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.ecom.be.common.mappers"})
 public class EcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
