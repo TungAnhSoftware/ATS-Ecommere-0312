68f2fc8a46d0ebf3d883cf1ef11b55a729d44516
Thanh Tung Ha <tung.ht3011@gmail.com>
2023-05-27 23:21:09 +0100

    [add] swagger dependencies

--- ecom-be-230312/src/main/java/com/ecom/be/EcommerceApplication.java
+++ ecom-be-230312/src/main/java/com/ecom/be/EcommerceApplication.java
@@ -2,10 +2,8 @@ package com.ecom.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.ecom.be.common.mappers"})
 public class EcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
