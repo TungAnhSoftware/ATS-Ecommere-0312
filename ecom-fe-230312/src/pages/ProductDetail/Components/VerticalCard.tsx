e720ba9ab61075e6632a2311e5f0d85ad6b19785
tnam0x <namtran4194@gmail.com>
2023-06-25 10:44:07 +0700

    change centercard style and others

--- ecom-fe-230312/src/pages/ProductDetail/Components/VerticalCard.tsx
+++ ecom-fe-230312/src/pages/ProductDetail/Components/VerticalCard.tsx
@@ -1,21 +1,21 @@
-import { Anchor, Card, Image, Stack, Title } from "@mantine/core";
+import { Anchor, Image, Stack, Title } from "@mantine/core";
 import CenterCard from "./CenterCard";
 
 interface VerticalCardProps {
   title: string,
-  desc: string,
+  subtitle: string,
   link: string,
-  text: string,
+  linkDesc: string,
   imgUrl: string | null,
 }
 
-const VerticalCard = ({ title, desc, link, text, imgUrl }: VerticalCardProps) => {
+const VerticalCard = ({ title, subtitle, link, linkDesc, imgUrl }: VerticalCardProps) => {
   return (
     <CenterCard w={350} h={500}>
       <Stack align="center">
-        <Title align="center" fw={700} fz='xl' style={{ whiteSpace: 'pre-wrap' }}>{title}</Title>
-        <Title align="center" fw={300} fz='lg'>{desc}</Title>
-        <Anchor align="center" href={link} target="_blank" >{text}</Anchor>
+        <Title align="center" fw={700} fz='xl' style={{ lineHeight: '1.1', whiteSpace: 'pre-wrap' }}>{title}</Title>
+        <Title align="center" fw={300} fz='md' style={{ lineHeight: '1.1' }}>{subtitle}</Title>
+        <Anchor align="center" href={link} target="_blank" >{linkDesc} &gt;</Anchor>
         <Image src={imgUrl} withPlaceholder />
       </Stack>
     </CenterCard>
--- ecom-fe-230312/src/pages/ProductDetail/ProductDetail.tsx
+++ ecom-fe-230312/src/pages/ProductDetail/ProductDetail.tsx
@@ -23,12 +23,7 @@ const ProductDetail = () => {
         <Space h="sm" />
         <Title order={2}>iPhone 14 and iPhone 14 Plus</Title>
         <Text fz="lg">From $799 or $33.29/mo. for 24 mo. before trade‑in*</Text>
-        <Button
-          onClick={handleBuyItem}
-          radius="xl"
-          size="sm"
-          style={{ cursor: 'pointer' }}
-        >
+        <Button onClick={handleBuyItem} radius="xl" size="sm" style={{ cursor: 'pointer' }}>
           Buy
         </Button>
       </center>
@@ -37,44 +32,28 @@ const ProductDetail = () => {
 
       <Group position="center">
         <CenterCard>
-          <Text align="center" fw={500} size={28}>
-            Big and Bigger.
-          </Text>
+          <Text align="center" fw={500} size={28}>Big and Bigger.</Text>
           <Image src={null} width={300} height={350} withPlaceholder />
 
-          <Space h="sm" />
+          <Space h='sm' />
 
           <Group position="center">
             <Stack spacing={0} align="center">
-              <Text size="lg" fw={500} color="dimmed">
-                iPhone 14
-              </Text>
-              <Text size={25} fw={700}>
-                6.1″
-              </Text>
+              <Text size='lg' fw={500} color="dimmed">iPhone 14</Text>
+              <Text size={25} fw={700}>6.1″</Text>
             </Stack>
             <Stack spacing={0} align="center">
-              <Text size="lg" fw={500} color="dimmed">
-                iPhone 14 Plus
-              </Text>
-              <Text size={25} fw={700}>
-                6.7″
-              </Text>
+              <Text size='lg' fw={500} color="dimmed">iPhone 14 Plus</Text>
+              <Text size={25} fw={700}>6.7″</Text>
             </Stack>
           </Group>
         </CenterCard>
-
         <Stack>
           <CenterCard mih={250}>
-            <Text align="center" fw={500} fz="lg" color="dimmed">
-              iPhone 14 Plus
-            </Text>
+            <Text align="center" fw={500} fz='lg' color="dimmed">iPhone 14 Plus</Text>
             <Text
-              align="center"
-              fw={500}
-              fz="xl"
-              variant="gradient"
-              gradient={{ from: 'green', to: 'darkgreen', deg: 45 }}
+              align="center" fw={500} fz='xl'
+              variant="gradient" gradient={{ from: 'green', to: 'darkgreen', deg: 45 }}
             >
               The looongest battery life of any iPhone. Ever.
             </Text>
@@ -82,131 +61,75 @@ const ProductDetail = () => {
           <CenterCard mih={250}>
             <Stack spacing={0} align="center">
               <Image src={null} width={150} height={150} withPlaceholder />
-              <Text fw={500} fz="lg" color="dimmed">
-                Ceramic Shield
-              </Text>
-              <Text fw={500} fz="xl">
-                Tougher than any smartphone glass.
-              </Text>
+              <Text fw={500} fz='lg' color="dimmed">Ceramic Shield</Text>
+              <Text fw={500} fz='xl'>Tougher than any smartphone glass.</Text>
             </Stack>
           </CenterCard>
         </Stack>
       </Group>
 
-      <Space h="md" />
+      <Space h='md' />
 
       <Group position="center">
         <CenterCard mih={250}>
-          <Text
-            fw={700}
-            fz="xl"
-            variant="gradient"
-            gradient={{ from: 'purple', to: 'pink', deg: 45 }}
-          >
-            Water resistance.
-          </Text>
+          <Text fw={700} fz='xl' variant="gradient" gradient={{ from: 'purple', to: 'pink', deg: 45 }}>Water resistance.</Text>
           <Text align="center">(Phew.)</Text>
         </CenterCard>
         <CenterCard>
-          <Text fw={500} fz="lg" color="dimmed" align="center">
-            Action mode
-          </Text>
-          <Text fw={500} fz="xl" color="grey" align="center">
-            Shaky shots, stable video.
-          </Text>
-          <Space h="sm" />
+          <Text fw={500} fz='lg' color="dimmed" align="center">Action mode</Text>
+          <Text fw={500} fz='xl' color="grey" align="center">Shaky shots, stable video.</Text>
+          <Space h='sm' />
           <Image src={null} width={300} height={150} withPlaceholder />
         </CenterCard>
       </Group>
 
-      <Space h="md" />
+      <Space h='md' />
 
       <Group position="center">
-        <Stack spacing="xs">
+        <Stack spacing='xs'>
           <CenterCard mih={150}>
-            <Text
-              fw={700}
-              fz="xl"
-              variant="gradient"
-              gradient={{ from: 'purple', to: 'pink', deg: 45 }}
-            >
-              A15 Bionic chip.
-            </Text>
+            <Text fw={700} fz='xl' variant="gradient" gradient={{ from: 'purple', to: 'pink', deg: 45 }}>A15 Bionic chip.</Text>
             <Text align="center">Fast that lasts.</Text>
           </CenterCard>
           <CenterCard mih={150}>
-            <Text
-              align="center"
-              fw={700}
-              fz="xs"
-              variant="gradient"
-              gradient={{ from: 'purple', to: 'pink', deg: 45 }}
-            >
-              Superspeedy
-            </Text>
-            <Text
-              align="center"
-              fw={700}
-              fz="xl"
-              variant="gradient"
-              gradient={{ from: 'purple', to: 'pink', deg: 45 }}
-            >
-              5G
-            </Text>
+            <Text align="center" fw={700} fz='xs' variant="gradient" gradient={{ from: 'purple', to: 'pink', deg: 45 }}>Superspeedy</Text>
+            <Text align="center" fw={700} fz='xl' variant="gradient" gradient={{ from: 'purple', to: 'pink', deg: 45 }}>5G</Text>
           </CenterCard>
         </Stack>
         <CenterCard>
-          <Text fw={500} fz="lg" align="center">
-            qq Pro-level camera.
-          </Text>
-          <Text fw={500} fz="xl" align="center">
-            Whoa-level pics.
-          </Text>
-          <Space h="sm" />
+          <Text fw={500} fz='lg' align="center">Pro-level camera.</Text>
+          <Text fw={500} fz='xl' align="center">Whoa-level pics.</Text>
+          <Space h='sm' />
           <Image src={null} width={300} height={200} withPlaceholder />
         </CenterCard>
       </Group>
 
-      <Space h="sm" />
+      <Space h='sm' />
 
       <Group position="center">
         <CenterCard>
           <Image src={null} height={150} withPlaceholder />
-          <Text fw={500} fz="xl">
-            Emergency SOS via satellite
-          </Text>
+          <Text fw={500} fz='xl'>Emergency SOS via satellite</Text>
         </CenterCard>
         <CenterCard>
           <Image src={null} width={150} height={150} withPlaceholder />
-          <Text fw={700} fz="xl">
-            Crash Detection
-          </Text>
+          <Text fw={700} fz='xl'>Crash Detection</Text>
         </CenterCard>
       </Group>
 
-      <Space h="sm" />
+      <Space h='sm' />
 
       <Group position="center">
         <CenterCard>
-          <Text fw={500} fz="md" color="dimmed" align="center">
-            Personalization
-          </Text>
-          <Text fw={700} fz="xl" align="center">
-            Your photo. Your font.
-          </Text>
-          <Text fw={700} fz="xl" align="center">
-            Your widgets. Your iPhone.
-          </Text>
+          <Text fw={500} fz='md' color="dimmed" align="center">Personalization</Text>
+          <Text fw={700} fz='xl' align="center">Your photo. Your font.</Text>
+          <Text fw={700} fz='xl' align="center">Your widgets. Your iPhone.</Text>
           <Image src={null} height={150} withPlaceholder />
         </CenterCard>
         <CenterCard>
           <Image src={null} height={150} withPlaceholder />
-          <Text fw={700} fz="xl" align="center">
-            Sharper, smarter,
-          </Text>
-          <Text fw={700} fz="xl" align="center">
-            snappier selfies.
-          </Text>
+          <Text fw={700} fz='xl' align="center">Sharper, smarter,</Text>
+          <Text fw={700} fz='xl' align="center">snappier selfies.</Text>
         </CenterCard>
       </Group>
 
@@ -219,9 +142,9 @@ const ProductDetail = () => {
           return (
             <VerticalCard
               title={item['title']}
-              desc={item['desc']}
+              subtitle={item['desc']}
               link={item['link']}
-              text={item['text']}
+              linkDesc={item['text']}
               imgUrl={null}
             />
           );
@@ -237,9 +160,9 @@ const ProductDetail = () => {
           return (
             <VerticalCard
               title={item['title']}
-              desc={item['desc']}
+              subtitle={item['desc']}
               link={item['link']}
-              text={item['text']}
+              linkDesc={item['text']}
               imgUrl={null}
             />
           );
